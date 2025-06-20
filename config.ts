0c4e14536fa4766e7f9ed0f5d936799eca3d01d4
Brendan Potter <84762913+brendanpotter00@users.noreply.github.com>
2025-06-19 22:55:43 -0500

    desc

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = "show interest in rankr";
 export const SITE_DESCRIPTION =
-  "humans are bad at ranking things. we're fixing that.";
+  "humans are bad at ranking things. we are fixing that and making it shareable.";
 export const PROJECT_TAG = "rankr";
