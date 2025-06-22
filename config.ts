a73cef3328f723f5814c3c1858122b74d0599747
Brendan Potter <84762913+brendanpotter00@users.noreply.github.com>
2025-06-22 18:12:39 -0500

    google ana token

--- config.ts
+++ config.ts
@@ -5,4 +5,4 @@ export const DEFAULT_SITE_DESCRIPTION = "Get notified when we launch.";
 // Google Analytics Configuration
 // Replace with your actual Google Analytics 4 Measurement ID
 export const GA_MEASUREMENT_ID =
-  process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || "G-";
+  process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || "G-EF8GHM35LY";
